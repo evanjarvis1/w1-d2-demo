72df3dbef5e0014e1adda276ee187867df521c11
Evan Jarvis <evanlegee@gmail.com>
2023-02-28 14:41:35 -0800

    Added demo file

---
+++ intro.js
@@ -0,0 +1,38 @@
+
+
+//console.log("Hello, World!")
+
+const firstName = `Evan`
+
+// console.log(firstName)
+
+let age = 23
+
+const isCool = true
+
+// console.log(firstName, age, isCool)
+
+// console.log("Hello, my name is " + firstName + " I am " + age + " years old.")
+
+// console.log(`Hello, my name is ${firstName} I am ${age} years old.`)
+
+let x = 4
+let y = 10
+let z = x + 8
+let sum = x + y + z
+
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+// console.log(sum)
+
+// if(condition to check){
+//     code to run
+// }
+
+if(x === y){
+    console.log(`X and Y have the same value`)
+} else {
+    console.log(`X and Y are different values`)
+}
